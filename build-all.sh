6480f1e16f3597a2f6f16bbacb78704ac77e9009
Greg Thompson <rencon@gmail.com>
2017-01-07 16:11:17 -0500

    updated configs

--- build-all.sh
+++ build-all.sh
@@ -2,17 +2,6 @@
 
 mkdir -p build
 
-echo "Building Instructor Deck"
-cp book.instructor-slides.json book.json
-gitbook build . website
-gitbook pdf .
-gitbook epub .
-pandoc -f epub -t docx -o book.docx book.epub
-mv website build/instructor-slides
-mv book.pdf build/instructor-slides.pdf
-mv book.epub build/instructor-slides.epub
-mv book.docx build/instructor-slides.docx
-
 echo "Building Instructor Book"
 cp book.instructor.json book.json
 gitbook build . website
@@ -23,17 +12,6 @@ mv website build/instructor
 mv book.pdf build/instructor.pdf
 mv book.epub build/instructor.epub
 mv book.docx build/instructor.docx
-
-echo "Building Student Slides"
-cp book.student-slides.json book.json
-gitbook build . website
-gitbook pdf .
-gitbook epub .
-pandoc -f epub -t docx -o book.docx book.epub
-mv website build/student-slides
-mv book.pdf build/student-slides.pdf
-mv book.epub build/student-slides.epub
-mv book.docx build/student-slides.docx
 
 echo "Building Student Book"
 cp book.student.json book.json
@@ -48,3 +26,7 @@ mv book.docx build/student.docx
 
 echo "Finishing up"
 cp book.master.json book.json
+cd build
+echo "Hit ctrl+C when you are done with this book"
+http-server
+cd ..
